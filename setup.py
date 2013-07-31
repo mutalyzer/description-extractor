6061c783521753b8fe90827205ad48946be8c00b
J.F.J. Laros <j.f.j.laros@lumc.nl>
2013-07-31 10:23:31 +0200

    Finished packaging, you need to run install twice with setuptools, perhaps pip does a better job.

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
+# Patch for swig.
+from distutils.command.build import build as _build
+_build.sub_commands = [
+    ('build_ext', _build.has_ext_modules),
+    ('build_py', _build.has_pure_modules),
+    ('build_clib', _build.has_c_libraries),
+    ('build_scripts', _build.has_scripts)
+] 
+
+#from setuptools.command.bdist_egg import bdist_egg
+#old_run = bdist_egg.run
+#
+#def run(self):
+#    old_run(self)
+#    self.run_command("install_lib")
+#
+#bdist_egg.run = run
+
 import sys
 from setuptools import setup
 from distutils.core import Extension
-
-# Patch for swig.
-from distutils.command.build import build
-build.sub_commands = [('build_ext', build.has_ext_modules), ('build_py',
-    build.has_pure_modules), ('build_clib', build.has_c_libraries),
-    ('build_scripts', build.has_scripts)] 
 
 if sys.version_info < (2, 6):
     raise Exception('extractor requires Python 2.6 or higher.')
@@ -26,6 +38,7 @@ setup(
     name='extractor',
     ext_modules=[Extension('_extractor', ['extractor/extractor.i',
         'extractor/extractor.cc'], swig_opts=['-c++'])],
+    py_modules=['extractor.extractor'],
     version=distmeta.__version__,
     description=distmeta.usage[0],
     long_description=distmeta.__doc__,
