f0a29f759d6acecf6d4b653173af27b9dcdbd722
J.F.J. Laros <J.F.J.Laros@lumc.nl>
2016-03-02 15:49:24 +0100

    Added debugging tests.

---
+++ test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+
+from __future__ import unicode_literals
+
+import monoseq
+
+from extractor import describe
+
+
+ref = 'MAVLWRLSAVCGALGGRALLLRTPVVRPAHISAFLQDRPIPEWCGVQHIHLSPSHHSGSKAASLHWTSERVVSVLLLGLLPAAYLNPCSAMDYSLAAALTLHGHWGLGQVVTDYVHGDALQKAAKAGLLALSALTFAGLCYFNYHDVGICKAVAMLWKL*'
+alt = 'MAVLWRLSAVCGAPTARDRRPSSVASNSSGQTCSYLSISSGPTYPRMVWSAAHTLVTEPPFWLQGCISPLD*'
+#ref = 'MDYSLAAALTLHGH'
+#alt = 'MTIPWRSPHFHGH'
+
+#ref = 'PAAYLNPCSAMDYSLAAALTLHGHWGLGQVVTDYVHGDALQKAAKAGLLALSALTFAGLCYFNYHDVGICKAVAMLWKL*'
+#alt = 'PAAYLNPCSAMTIPWLQPSLFMVTGALDKLLLTMFMGMPCRKLPRQGFWHFQL*'
+
+#ref = 'KGFRLLNPHPKPNPKNN*'
+#alt = 'KGFRLLNPHPSQTPKTTDIITQEIFATLVFFQHQQLLLNCQTQLAIYIETSVLKDL*'
+
+description = describe.describe_protein(ref, alt)
+
+ref_annotation = []
+alt_annotation = []
+for rv in description:
+    for i in rv.annotated_inserted:
+        ref_annotation.append([(i.start - 1, i.end)])
+        alt_annotation.append([(i.sample_start - 1, i.sample_end)])
+        print '({}, {}), {}, ({}, {})'.format(
+            i.start, i.end, rv.start, i.sample_start, i.sample_end)
+
+print '    p.{}'.format(description)
+print '    q.{}'.format(description.nhgvs())
+
+print
+print monoseq.pprint_sequence(ref, ref_annotation, format=monoseq.AnsiFormat)
+print
+print monoseq.pprint_sequence(alt, alt_annotation, format=monoseq.AnsiFormat)
+print
