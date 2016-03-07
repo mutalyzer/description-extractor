677ed7c0793228ba88a7e74c3d12dd0b8a4079bd
jkvis <j.k.vis@lumc.nl>
2016-03-07 11:51:15 +0100

    Added python 3.5

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: C++',
         'Topic :: Scientific/Engineering',
     ],
