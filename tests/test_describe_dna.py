8d286cf835466c321200d475199d6fe50a89f53a
jkvis <j.k.vis@lumc.nl>
2019-01-24 12:05:36 +0100

    Old tests ported to new dict output for single variants

--- tests/test_describe_dna.py
+++ tests/test_describe_dna.py
@@ -14,7 +14,9 @@ TESTS = [
                     'type': 'range'},
        'type': 'equal'}]),
 
-    # Single substitution: 7A>G
+    # Single variants
+
+    ## Substitution: 7A>G
     (REFERENCE,
      'ACGTCGGTTCGCTAGCTTCGGGGGATAGATAGAGATATAGAGAT',
      [{'location': {'start': {'position': 0, 'type': 'point'},
@@ -34,7 +36,7 @@ TESTS = [
                     'type': 'range'},
         'type': 'equal'}]),
 
-    # Single deletion: 7del
+    ## Single nucleotide deletion: 7del
     (REFERENCE,
      'ACGTCGTTCGCTAGCTTCGGGGGATAGATAGAGATATAGAGAT',
      [{'location': {'start': {'position': 0, 'type': 'point'},
@@ -54,54 +56,157 @@ TESTS = [
                     'type': 'range'},
         'type': 'equal'}]),
 
-]
-
-
-UNUSED = [
-    # Single deletion: 7del
-    (REFERENCE,
-     'ACGTCGTTCGCTAGCTTCGGGGGATAGATAGAGATATAGAGAT',
-     [{}]),
-
-    # Single deletion multiple bases: 7_8del
+    ## Multi nucleotide deletion: 7_8del
     (REFERENCE,
      'ACGTCGTCGCTAGCTTCGGGGGATAGATAGAGATATAGAGAT',
-     [{}]),
+     [{'location': {'start': {'position': 0, 'type': 'point'},
+                    'end':   {'position': 6, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'},
+      {'location': {'start': {'position': 6, 'type': 'point'},
+                    'end':   {'position': 8, 'type': 'point'},
+                    'type': 'range'},
+        'insertions': [{'source': 'observed',
+                        'location': {'start': {'position': 6, 'type': 'point'},
+                                     'end'  : {'position': 6, 'type': 'point'},
+                        'type': 'range'}}],
+        'type': 'delins'},
+      {'location': {'start': {'position':  8, 'type': 'point'},
+                    'end':   {'position': 44, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'}]),
 
-    # Single insertion: 6_7insC
+    ## Single nucleotide insertion: 6_7insC
     (REFERENCE,
      'ACGTCGCATTCGCTAGCTTCGGGGGATAGATAGAGATATAGAGAT',
-     [{}]),
+     [{'location': {'start': {'position': 0, 'type': 'point'},
+                    'end':   {'position': 6, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'},
+      {'location': {'start': {'position': 6, 'type': 'point'},
+                    'end':   {'position': 6, 'type': 'point'},
+                    'type': 'range'},
+        'insertions': [{'source': 'observed',
+                        'location': {'start': {'position': 6, 'type': 'point'},
+                                     'end'  : {'position': 7, 'type': 'point'},
+                        'type': 'range'}}],
+        'type': 'delins'},
+      {'location': {'start': {'position':  6, 'type': 'point'},
+                    'end':   {'position': 44, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'}]),
 
-    # Single insertion multiple bases: 6_7insCC
+    ## Multi nucleotide insertion: 6_7insCC
     (REFERENCE,
      'ACGTCGCCATTCGCTAGCTTCGGGGGATAGATAGAGATATAGAGAT',
-     [{}]),
+     [{'location': {'start': {'position': 0, 'type': 'point'},
+                    'end':   {'position': 6, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'},
+      {'location': {'start': {'position': 6, 'type': 'point'},
+                    'end':   {'position': 6, 'type': 'point'},
+                    'type': 'range'},
+        'insertions': [{'source': 'observed',
+                        'location': {'start': {'position': 6, 'type': 'point'},
+                                     'end'  : {'position': 8, 'type': 'point'},
+                        'type': 'range'}}],
+        'type': 'delins'},
+      {'location': {'start': {'position':  6, 'type': 'point'},
+                    'end':   {'position': 44, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'}]),
 
-    # Single inversion: 7_11inv
+    ## Inversion: 7_11inv
     (REFERENCE,
      'ACGTCGCGAATCTAGCTTCGGGGGATAGATAGAGATATAGAGAT',
-     [{}]),
+     [{'location': {'start': {'position': 0, 'type': 'point'},
+                    'end':   {'position': 6, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'},
+      {'location': {'start': {'position': 6, 'type': 'point'},
+                    'end':   {'position': 11, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'inv'},
+      {'location': {'start': {'position': 11, 'type': 'point'},
+                    'end':   {'position': 44, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'}]),
 
-    # Single delins: 7delinsCC
-    (REFERENCE,
-     'ACGTCGCCTTCGCTAGCTTCGGGGGATAGATAGAGATATAGAGAT',
-     [{}]),
-
-    # Single delins: 21_23delinsTTTT
-    (REFERENCE,
-     'ACGTCGATTCGCTAGCTTCGTTTTGATAGATAGAGATATAGAGAT',
-     [{}]),
-
-    # Single inversion: 6_7inv
+    ## Inversion: 6_7inv
     (REFERENCE,
      'ACGTCTCTTCGCTAGCTTCGGGGGATAGATAGAGATATAGAGAT',
-     [{}]),
+     [{'location': {'start': {'position': 0, 'type': 'point'},
+                    'end':   {'position': 5, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'},
+      {'location': {'start': {'position': 5, 'type': 'point'},
+                    'end':   {'position': 7, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'inv'},
+      {'location': {'start': {'position': 7, 'type': 'point'},
+                    'end':   {'position': 44, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'}]),
 
-    # Single inversion: 7_8delinsTC
+    ## Deletion/insertion 7delinsCC
+    (REFERENCE,
+     'ACGTCGCCTTCGCTAGCTTCGGGGGATAGATAGAGATATAGAGAT',
+     [{'location': {'start': {'position': 0, 'type': 'point'},
+                    'end':   {'position': 6, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'},
+      {'location': {'start': {'position': 6, 'type': 'point'},
+                    'end':   {'position': 7, 'type': 'point'},
+                    'type': 'range'},
+        'insertions': [{'source': 'observed',
+                        'location': {'start': {'position': 6, 'type': 'point'},
+                                     'end'  : {'position': 8, 'type': 'point'},
+                        'type': 'range'}}],
+        'type': 'delins'},
+      {'location': {'start': {'position':  7, 'type': 'point'},
+                    'end':   {'position': 44, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'}]),
+
+    ## Deletion/insertion 21_23delinsTTTT
+    (REFERENCE,
+     'ACGTCGATTCGCTAGCTTCGTTTTGATAGATAGAGATATAGAGAT',
+     [{'location': {'start': {'position': 0, 'type': 'point'},
+                    'end':   {'position': 20, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'},
+      {'location': {'start': {'position': 20, 'type': 'point'},
+                    'end':   {'position': 23, 'type': 'point'},
+                    'type': 'range'},
+        'insertions': [{'source': 'observed',
+                        'location': {'start': {'position': 20, 'type': 'point'},
+                                     'end'  : {'position': 24, 'type': 'point'},
+                        'type': 'range'}}],
+        'type': 'delins'},
+      {'location': {'start': {'position': 23, 'type': 'point'},
+                    'end':   {'position': 44, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'}]),
+
+    ## Deletion/insertion 7_8delinsTC
     (REFERENCE,
      'ACGTCGTCTCGCTAGCTTCGGGGGATAGATAGAGATATAGAGAT',
-     [{}]),
+     [{'location': {'start': {'position': 0, 'type': 'point'},
+                    'end':   {'position': 6, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'},
+      {'location': {'start': {'position': 6, 'type': 'point'},
+                    'end':   {'position': 8, 'type': 'point'},
+                    'type': 'range'},
+        'insertions': [{'source': 'observed',
+                        'location': {'start': {'position': 6, 'type': 'point'},
+                                     'end'  : {'position': 8, 'type': 'point'},
+                        'type': 'range'}}],
+        'type': 'delins'},
+      {'location': {'start': {'position': 8, 'type': 'point'},
+                    'end':   {'position': 44, 'type': 'point'},
+                    'type': 'range'},
+        'type': 'equal'}]),
 
 ]
 
